3db6333143d3ac7b1ea78f9fbe0f169383e65fb5
fm <81640753+eraflo@users.noreply.github.com>
2025-02-05 19:08:21 +0200

    Change stats affected by food

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -5,8 +5,8 @@ signal touchscreen_input(event: InputEventScreenTouch)
 const TouchscreenCamera = preload("res://Assets/Scripts/TouchscreenCamera.gd")
 
 
-@export var weight: float = 50.0
-@export var height: float = 1.5
+@export var weight: PlayerStat
+@export var height: PlayerStat
 
 # Movement
 @export var gravity: float = 980
@@ -17,12 +17,18 @@ const TouchscreenCamera = preload("res://Assets/Scripts/TouchscreenCamera.gd")
 
 @export var max_speed: PlayerStat
 @export var jump_force: PlayerStat
+@export var jump_delay: PlayerStat 
 @export var friction: PlayerStat
 @export var falling_speed: PlayerStat
 @export var bounce_factor: PlayerStat
-@export var stat_drain: PlayerStat
-@export var vision_fog: PlayerStat
 @export var stun_duration: PlayerStat
+
+var _sugar: PlayerStat
+var _protein: PlayerStat
+var _fat: PlayerStat
+var _water: PlayerStat
+var _fiber: PlayerStat
+var _vitamin: PlayerStat
 
 # State manager
 @onready var stateManager = $StateManager
@@ -38,7 +44,7 @@ func _ready() -> void:
 	camera = get_parent().get_node("TouchscreenCamera") as TouchscreenCamera
 	camera.get_touchscreen_input.connect(_on_touchscreen_input)
 
-	_reset()
+	reset()
 	
 	start()
 	
@@ -61,85 +67,70 @@ func _process(delta: float) -> void:
 		
 	move_and_slide()
 
+## Make the player began behaving as a player
+func start() -> void:
+	stateManager.request_state("Run")
+
+## Reset the player stats
+func reset():
+	_setup_stats()
+
+## Stop the player from behaving as a player
+func stop() -> void:
+	stateManager.request_state("Idle")
+
+## Eat a food
+func eat_food(food: Food) -> void:
+	food_eaten.append(food)
+
+	_add_modifier(_sugar, food.sugar, StatModifier.StatModType.Flat)
+	_add_modifier(_protein, food.protein, StatModifier.StatModType.Flat)
+	_add_modifier(_fat, food.fat, StatModifier.StatModType.Flat)
+	_add_modifier(_water, food.water, StatModifier.StatModType.Flat)
+	_add_modifier(_fiber, food.fiber, StatModifier.StatModType.Flat)
+	_add_modifier(_vitamin, food.vitamin, StatModifier.StatModType.Flat)
+
+## Handle the touchscreen input
 func _on_touchscreen_input(event: InputEventScreenTouch) -> void:
 	print("Player: Touchscreen input")
 	touchscreen_input.emit(event)
 
-## Reset the player stats
-func _reset():
-	# Set the initial values for the Health System
-	Health.weight = weight
-	Health.height = height
 
-	# Add stat drain modifier to each stat
-	max_speed.add_modifier(StatModifier.new(
-		-stat_drain.value,
-		StatModifier.StatModType.Flat
+## Add a modifier to a stat
+func _add_modifier(stat: PlayerStat, value: float, type: StatModifier.StatModType) -> void:
+	stat.add_modifier(StatModifier.new(
+		value,
+		type
 	))
 
-	jump_force.add_modifier(StatModifier.new(
-		-stat_drain.value,
-		StatModifier.StatModType.Flat
-	))
+## Setup the stats based on the food eaten
+func _setup_stats() -> void:
 
-	friction.add_modifier(StatModifier.new(
-		-stat_drain.value,
-		StatModifier.StatModType.Flat
-	))
+	# Setup food stats
+	_sugar = PlayerStat.new()
+	_protein = PlayerStat.new()
+	_fat = PlayerStat.new()
+	_water = PlayerStat.new()
+	_fiber = PlayerStat.new()
+	_vitamin = PlayerStat.new()
 
-	falling_speed.add_modifier(StatModifier.new(
-		stat_drain.value,
-		StatModifier.StatModType.Flat
-	))
+	# Set the base values for the food stats (TODO: Ask Xiaolan for right ratios)
+	_sugar.base_value = 10
+	_protein.base_value = 10
+	_fat.base_value = 10
+	_water.base_value = 10
+	_fiber.base_value = 10
+	_vitamin.base_value = 10
+	
+	# Set the stats based on the food eaten
+	_add_modifier(max_speed, _sugar.value - _protein.value - _fat.value + _water.value, StatModifier.StatModType.Flat)
+	_add_modifier(jump_force, _protein.value - _fat.value + _water.value, StatModifier.StatModType.Flat)
+	_add_modifier(jump_delay, _sugar.value, StatModifier.StatModType.Flat)
+	_add_modifier(friction, -_fat.value, StatModifier.StatModType.Flat)
+	_add_modifier(falling_speed, _fat.value, StatModifier.StatModType.Flat)
+	_add_modifier(bounce_factor, _water.value, StatModifier.StatModType.Flat)
+	_add_modifier(stun_duration, _vitamin.value, StatModifier.StatModType.Flat)
 
-	bounce_factor.add_modifier(StatModifier.new(
-		stat_drain.value,
-		StatModifier.StatModType.Flat
-	))
+	# Weight
+	_add_modifier(weight, _sugar.value + _protein.value + _fat.value + _water.value + _fiber.value + _vitamin.value, StatModifier.StatModType.Flat)
 
-	stun_duration.add_modifier(StatModifier.new(
-		stat_drain.value,
-		StatModifier.StatModType.Flat
-	))
-
-
-func stop() -> void:
-	stateManager.request_state("Idle")
-
-func start() -> void:
-	stateManager.request_state("Run")
-
-func eat_food(food: Food) -> void:
-	food_eaten.append(food)
-
-	Health.add_weight(food.weight)
-
-	max_speed.add_modifier(StatModifier.new(
-		food.sugar - food.protein - food.fat + food.water,
-		StatModifier.StatModType.Flat
-	))
-
-	jump_force.add_modifier(StatModifier.new(
-		food.protein - food.fat + food.water,
-		StatModifier.StatModType.Flat
-	))
-
-	friction.add_modifier(StatModifier.new(
-		-food.fat,
-		StatModifier.StatModType.Flat
-	))
-
-	falling_speed.add_modifier(StatModifier.new(
-		food.fat,
-		StatModifier.StatModType.Flat
-	))
-
-	bounce_factor.add_modifier(StatModifier.new(
-		food.water,
-		StatModifier.StatModType.Flat
-	))
-
-	stun_duration.add_modifier(StatModifier.new(
-		food.vitamin,
-		StatModifier.StatModType.Flat
-	))
