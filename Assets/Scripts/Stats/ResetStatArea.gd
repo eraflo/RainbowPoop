1876665d827b1db80d61d4922be4d253669d6217
fm <81640753+eraflo@users.noreply.github.com>
2025-02-07 08:17:33 +0200

    Added enum for reset stat area

--- Assets/Scripts/Stats/ResetStatArea.gd
+++ Assets/Scripts/Stats/ResetStatArea.gd
@@ -2,27 +2,39 @@ extends Area2D
 
 const Player = preload("res://Assets/Scripts/Player.gd")
 
-@export var stat_to_reset: String = "weight"
+enum StatToReset {
+    weight,
+    height,
+    max_speed,
+    jump_force,
+    jump_delay,
+    friction,
+    falling_speed,
+    bounce_factor,
+    stun_duration
+}
+
+@export var stat_to_reset: StatToReset = StatToReset.weight
 
 func _on_body_entered(body: Node2D) -> void:
     if body is Player:
         match stat_to_reset:
-            "weight":
+            StatToReset.weight:
                 body.weight.remove_all_modifiers()
-            "height":
+            StatToReset.height:
                 body.height.remove_all_modifiers()
-            "max_speed":
+            StatToReset.max_speed:
                 body.max_speed.remove_all_modifiers()
-            "jump_force":
+            StatToReset.jump_force:
                 body.jump_force.remove_all_modifiers()
-            "jump_delay":
+            StatToReset.jump_delay:
                 body.jump_delay.remove_all_modifiers()
-            "friction":
+            StatToReset.friction:
                 body.friction.remove_all_modifiers()
-            "falling_speed":
+            StatToReset.falling_speed:
                 body.falling_speed.remove_all_modifiers()
-            "bounce_factor":
+            StatToReset.bounce_factor:
                 body.bounce_factor.remove_all_modifiers()
-            "stun_duration":
+            StatToReset.stun_duration:
                 body.stun_duration.remove_all_modifiers()
 
