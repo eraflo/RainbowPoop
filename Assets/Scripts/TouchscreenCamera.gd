a864ead0facb2105af8710d9dc7573968377794d
Heredos <heredos.me@gmail.com>
2025-02-17 14:54:17 +0200

    intro camera

--- Assets/Scripts/TouchscreenCamera.gd
+++ Assets/Scripts/TouchscreenCamera.gd
@@ -3,16 +3,29 @@ extends Camera2D
 signal get_touchscreen_input(event: InputEventScreenTouch)
 
 var player
+var path
+var startedAt
+var pathLength
 
 func _ready() -> void:
 	var parent = get_parent()
 	player = parent.find_child("Player", false)
-	
+	path = parent.find_child("Guide", false).curve
+	position = path.get_point_position(0)
+	zoom = Vector2(0.15, 0.15)
+	startedAt = Time.get_unix_time_from_system()
+	pathLength = path.get_baked_length()
 
 func _input(event):
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			get_touchscreen_input.emit(event)
 func _process(delta: float) -> void:
-	position = player.position
-	print(get_viewport().get_screen_transform())
+	var timeSinceStart = Time.get_unix_time_from_system()-startedAt
+	if (timeSinceStart < 20):
+		var coef=1-(timeSinceStart/20)
+		zoom=Vector2(1-(coef*0.75), 1-(coef*0.75))
+		position = path.sample_baked(((1-coef)*pathLength), true)
+	else:
+		position = player.position
+	# print(get_viewport().get_screen_transform())
