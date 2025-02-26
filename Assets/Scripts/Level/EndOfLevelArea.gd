62466f14e24e7744bc60623d496327b327f06aae
Heredos <heredos.me@gmail.com>
2025-02-26 23:24:21 +0200

    added the animation on the end screen

--- Assets/Scripts/Level/EndOfLevelArea.gd
+++ Assets/Scripts/Level/EndOfLevelArea.gd
@@ -32,6 +32,15 @@ func _on_body_entered(_body: Node2D) -> void:
 		# Save the score for the level
 		var level_node = get_tree().root.get_node(level_name)
 		level_node.save_level_completed()
-
+		
+		# loads the food detail inside the ScoreDetail singleton
+		ScoreDetail.sugar = _body._sugar.value
+		ScoreDetail.fat = _body._fat.value
+		ScoreDetail.fiber = _body._fiber.value
+		ScoreDetail.protein = _body._protein.value
+		ScoreDetail.vitamin = _body._vitamin.value
+		ScoreDetail.water = _body._water.value
+		
+		
 		# TODO: Menu to go to the next level
 		get_tree().change_scene_to_file("res://Assets/Scenes/EndAnimation.tscn")
